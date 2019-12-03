e17f81b908b6d0d218e4dde6bec9f21a4a4df7ec
DavidMaxwell616 <33132201+DavidMaxwell616@users.noreply.github.com>
2019-12-02 21:50:12 -0800

    updates

--- js/test.js
+++ js/test.js
@@ -23,61 +23,62 @@ var game = new Phaser.Game(config);
 
 function preload() {
   this.load.path = '../assets/json/';
-  this.load.json('levelData', 'sprite_physics.json');
+  this.load.json('levelData', 'level_data.json');
 
   this.load.path = '../assets/images/';
   this.load.image('level 1', 'level_1.png');
 }
 
 function create() {
-  let data = this.cache.json.get('levelData').level_1;
-  for (let index = 0; index < data.length; index++) {
+  loadLevel(this, 1);
+}
+
+function loadLevel(scene, level) {
+  let data = scene.cache.json.get('levelData');
+  let levelData = data['level_1'];
+  for (let index = 0; index < levelData.length; index++) {
     var level = {
       polygons: [],
     };
-    var vertices = data[index].shape;
-    var xScale = 1;
-    var yScale = 1;
-    var x = vertices[0] * xScale;
-    var y = vertices[1] * yScale;
-    var path = getOffsets(x, y, xScale, yScale, vertices);
-    var verts = this.matter.verts.fromPath(path);
-    var poly = this.add.polygon(x, y, verts, 0x0000ff);
-    var body = this.matter.add
+    var vertices = levelData[index].shape;
+    let polyObject = [];
+    for (let i = 0; i < vertices.length / 2; i++) {
+      polyObject.push({
+        x: vertices[i * 2],
+        y: vertices[i * 2 + 1],
+      });
+    }
+
+    let centre = Phaser.Physics.Matter.Matter.Vertices.centre(
+      polyObject,
+    );
+    var verts = scene.matter.verts.fromPath(vertices.join(' '));
+    const xScale = .9;
+    const yScale = .7;
+    for (let i = 0; i < verts.length; i++) {
+      ((verts[i].x -= centre.x) * -1) * xScale;
+      ((verts[i].y -= centre.y) * -1) * yScale;
+    }
+    var poly = scene.add.polygon(
+      centre.x * xScale,
+      centre.y * yScale,
+      verts,
+      0x0000ff
+    );
+    level.polygons.push(poly);
+    scene.matter.add
       .gameObject(poly, {
         shape: {
           type: 'fromVerts',
           verts,
           flagInternal: true,
-          density: 5,
-          friction: 1,
-          frictionAir: 1,
-          frictionStatic: 1,
-          restitution: 0
         },
       })
-      .setStatic(true)
       .setOrigin(0, 0);
-    // level1bkgd = this.add.image(0, 0, 'level 1');
-    // level1bkgd.setOrigin(0, 0);
-    // level1bkgd.setDisplaySize(
-    //   this.game.config.width,
-    //   this.game.config.height * 0.7,
-    // );
+
   }
-  //   levelData.push(level);
 }
 
-function getOffsets(xOffset, yOffset, xScale, yScale, vertices) {
-  var newPath = [];
-
-  for (let index = 0; index < vertices.length; index += 2) {
-    newPath.push((vertices[index] - xOffset) * xScale);
-    newPath.push((vertices[index + 1] - yOffset) * yScale);
-  }
-  console.log(xOffset, yOffset, newPath);
-  return newPath.toString();
-}
 
 function update() {
 
